448c148b5e5381be2b50a1b01c62f235404b00fa
yuzu-juice <yuzujuicesec@gmail.com>
2025-11-04 14:07:35 +0900

    add: leaf

---
+++ init.el
@@ -0,0 +1,35 @@
+;; <leaf-install-code>
+(eval-and-compile
+  (customize-set-variable
+   'package-archives '(("org" . "https://orgmode.org/elpa/")
+                       ("melpa" . "https://melpa.org/packages/")
+                       ("gnu" . "https://elpa.gnu.org/packages/")))
+  (package-initialize)
+  (unless (package-installed-p 'leaf)
+    (package-refresh-contents)
+    (package-install 'leaf))
+
+  (leaf leaf-keywords
+    :ensure t
+    :init
+    ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
+    (leaf hydra :ensure t)
+    (leaf el-get :ensure t)
+    (leaf blackout :ensure t)
+
+    :config
+    ;; initialize leaf-keywords.el
+    (leaf-keywords-init)))
+;; </leaf-install-code>
+
+;; 現在位置表示
+(column-number-mode t)
+
+;; 行番号表示
+(global-display-line-numbers-mode t)
+
+;; 最初の挨拶非表示
+(setq inhibit-startup-message t)
+
+;; key bind
+(global-set-key (kbd "C-t") 'delete-window)
