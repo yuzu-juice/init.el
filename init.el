a40a573a2eee305de34f89b77ddc8e4fc3739eae
yuzu-juice <yuzujuicesec@gmail.com>
2025-11-28 16:19:28 +0900

    update: init.el

--- init.el
+++ init.el
@@ -1,170 +1,134 @@
 ;;; init.el --- setting file -*- lexical-binding: t -*-
-
 ;;; Author: yuzu-juice
 ;;; Commentary:
-
 ;;; Code:
-;; byte compile warning 非表示
+
+;; Suppress byte compile warnings
 (setq byte-compile-warnings nil)
 
-;; <leaf-install-code>
-(eval-and-compile
-  (customize-set-variable
-   'package-archives '(("org" . "https://orgmode.org/elpa/")
-                       ("melpa" . "https://melpa.org/packages/")
-                       ("gnu" . "https://elpa.gnu.org/packages/")))
-  (package-initialize)
-  (unless (package-installed-p 'leaf)
-    (package-refresh-contents)
-    (package-install 'leaf))
+;; Package archives and initialization
+(require 'package)
+(setq package-archives '(("org" . "https://orgmode.org/elpa/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
 
-  (leaf leaf-keywords
-    :ensure t
-    :config
-    ;; initialize leaf-keywords.el
-    (leaf-keywords-init)))
-
-;;
 ;; Core Emacs Settings
-;;
-(leaf emacs
-  :custom
-  ;; 現在位置表示
-  (column-number-mode . t)
-  ;; 行番号表示
-  (global-display-line-numbers-mode . t)
-  ;; 最初の挨拶非表示
-  (inhibit-startup-message . t)
-  ;; 最終行に空行を強制
-  (require-final-newline . t)
-  ;; 起動時に最後に開いていたファイルを開く
-  (desktop-save-mode . 1)
-  ;; バッファの自動リフレッシュ
-  (global-auto-revert-mode . 1)
+(setq column-number-mode t)
+(global-display-line-numbers-mode 1)
+(setq inhibit-startup-message t)
+(setq require-final-newline t)
+(desktop-save-mode 1)
+(global-auto-revert-mode 1)
+
+;; Cursor color and coding system
+(set-cursor-color "#F79428")
+(prefer-coding-system 'utf-8)
+(setq compilation-scroll-output t)
+
+;; Keybindings
+(global-set-key (kbd "C-t") 'delete-window)
+(global-set-key (kbd "C-h") 'delete-backward-char)
+
+;; UI Packages
+(use-package doom-themes
+  :ensure t
   :config
-  ;; カーソルの色を変更
-  (set-cursor-color "#F79428")
-  ;; UTF-8を優先
-  (prefer-coding-system 'utf-8)
-  :bind
-  (("C-t" . delete-window)
-   ("C-h" . delete-backward-char)))
+  (load-theme 'doom-solarized-light t))
 
-;;
-;; Ui Packages
-;;
-(leaf doom-themes
+(use-package doom-modeline
   :ensure t
-  :config (load-theme 'doom-solarized-light t))
+  :config (doom-modeline-mode 1))
 
-(leaf doom-modeline
+(use-package beacon
   :ensure t
-  :global-minor-mode t)
+  :config (beacon-mode 1)
+  :custom
+  (beacon-color "#FAB27B"))
 
-(leaf beacon
-  :ensure t
-  :global-minor-mode t
-  :custom ((beacon-color . "#FAB27B")))
-
-;;
 ;; Editing Packages
-;;
-(leaf undo-tree
+(use-package undo-tree
   :ensure t
   :hook ((prog-mode . undo-tree-mode)
-	 (markdown-mode . undo-tree-mode)
-	 (text-mode . undo-tree-mode))
+         (markdown-mode . undo-tree-mode)
+         (text-mode . undo-tree-mode))
   :custom
-  (undo-tree-auto-save-history . nil))
+  (undo-tree-auto-save-history nil))
 
-(leaf gcmh
+(use-package gcmh
   :ensure t
-  :global-minor-mode t
+  :config (gcmh-mode 1)
   :custom
-  (gcmh-verbose . t))
+  (gcmh-verbose t))
 
-;;
 ;; Programming Packages
-;;
-(leaf lsp-mode
+(use-package lsp-mode
   :ensure t
-  :hook ((c-mode . lsp)
-         (c++-mode . lsp)
-	 (typescript-ts-mode . lsp)
-	 (tsx-ts-mode . lsp)
-	 (typescript-mode . lsp)
-	 (js-mode . lsp)
-	 (js2-mode . lsp)
-	 (json-ts-mode . lsp))
-  :commands lsp)
+  :commands (lsp lsp-deferred)
+  :hook ((c-mode . lsp-deferred)
+         (c++-mode . lsp-deferred)
+         (typescript-ts-mode . lsp-deferred)
+         (tsx-ts-mode . lsp-deferred)
+         (typescript-mode . lsp-deferred)
+         (js-mode . lsp-deferred)
+         (js2-mode . lsp-deferred)
+         (json-ts-mode . lsp-deferred))
+  :custom
+  (lsp-use-plists t))
 
-(leaf lsp-ui
+(use-package lsp-ui
   :ensure t
   :after lsp-mode
-  :commands lsp-ui-mode
-)
+  :commands lsp-ui-mode)
 
-(leaf projectile
+(use-package projectile
   :ensure t
-  :global-minor-mode projectile-mode)
+  :config (projectile-mode 1))
 
-;;
 ;; Git Packages
-;;
-
-(leaf magit
+(use-package magit
   :ensure t
   :bind (("C-x g" . magit-status)))
 
-(leaf diff-hl
+(use-package diff-hl
   :ensure t
-  :hook
-  ((prog-mode . diff-hl-mode)
-   (magit-refresh-buffer . diff-hl-magit-post-refresh)))
+  :hook ((prog-mode . diff-hl-mode)
+         (magit-refresh-buffer . diff-hl-magit-post-refresh)))
 
-;;
 ;; File browser Packages
-;;
-
-(leaf treemacs
+(use-package treemacs
   :ensure t
   :custom
-  (treemacs-is-never-other-window . t)
-  :bind
-  ("<f8>" . treemacs)
-  (:treemacs-mode-map
-   ([mouse-1] . #'treemacs-single-click-expand-action))
-  )
+  (treemacs-is-never-other-window t)
+  :bind ("<f8>" . treemacs)
+  :config
+  (define-key treemacs-mode-map [mouse-1] #'treemacs-single-click-expand-action))
 
-(leaf treemacs-magit
+(use-package treemacs-magit
   :ensure t
   :after (treemacs magit))
 
-(leaf treemacs-projectile
+(use-package treemacs-projectile
   :ensure t
   :after (treemacs projectile)
   :config
   (add-hook 'projectile-after-switch-project-hook
             #'treemacs-display-current-project-exclusively))
 
-
-;;;
-;;; icons
-;;;
-(leaf nerd-icons
+;; Icons
+(use-package nerd-icons
   :ensure t)
 
-(leaf treemacs-nerd-icons
+(use-package treemacs-nerd-icons
   :ensure t
-  :after treemacs nerd-icons
+  :after (treemacs nerd-icons)
   :config
   (treemacs-nerd-icons-config))
 
-;;
 ;; Other Packages
-;;
-
-(leaf mozc
+(use-package mozc
   :ensure t
   :config
   (setq default-input-method "japanese-mozc"))
@@ -173,11 +137,11 @@
        (cond ((eq system-type 'darwin) "/opt/homebrew/bin/wakatime-cli")
              ((eq system-type 'gnu/linux)
               (expand-file-name "~/.wakatime/wakatime-cli"))))
-  (leaf wakatime-mode :ensure t :global-minor-mode t))
+  (use-package wakatime-mode
+    :ensure t
+    :config (global-wakatime-mode 1)))
 
-(leaf vterm
+(use-package vterm
   :ensure t)
-
-
 
 ;;; init.el ends here
